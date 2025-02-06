5560c80873b8f0889a53685ae30a4d0c1385c524
maggson <dieseverycingein@yandex.ru>
2025-02-06 16:50:46 +0300

    финал очка без коммов и без обработки ошибок

--- ClassLibrary/Farmer.cs
+++ ClassLibrary/Farmer.cs
@@ -7,11 +7,7 @@ namespace ClassLibrary
 {
     public static class Faremer
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="perimeter"></param>
-        /// <returns> Площадь ромба </returns>
+
         static double CorRhombus(double perimeter)
         {
             double side = perimeter / 4;
@@ -57,41 +53,6 @@ namespace ClassLibrary
         {
             double side = perimeter / sides;
             double area = (sides * side * side / 4) * (1 / Math.Tan(Math.PI / sides));
-            return area;
-        }
-        static double CorPentagon(double perimeter) 
-        {
-            double side = perimeter / 5;
-            double area = (5 * side * side / 2) * Math.Sqrt(5+2*Math.Sqrt(5));
-            return area;
-        }
-        static double CorHexagon(double perimeter)
-        {
-            double side = perimeter / 6;
-            double area = (3 * Math.Sqrt(3) / 2) * side * side;
-            return area;
-        }
-
-        static double CorRectangle(double perimeter)
-        {
-            double area = perimeter * perimeter / 18;
-            return area;
-        }
-        static double CorSquare(double perimeter)
-        {
-            double area = Math.Pow(perimeter / 4, 2);
-            return area;
-        }
-        static double CorTriangle(double perimeter)
-        {
-            double area = Math.Sqrt(3) * Math.Pow(perimeter, 2) / 36;
-            return area;
-        }
-
-        static double CorNgon(double perimeter, int sides)
-        {
-            double side = perimeter / sides;
-            double area = (sides*side*side/4) * (1/Math.Tan(Math.PI/sides));
             return area;
         }
     }
