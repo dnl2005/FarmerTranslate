3f9a27323e7303bbbe7b84713e54c3354c9f5bce
Holografic <holografic@dev.com>
2025-02-06 15:53:52 +0300

    c

--- ClassLibrary/Farmer.cs
+++ ClassLibrary/Farmer.cs
@@ -1,12 +1,22 @@
-﻿namespace ClassLibrary
+﻿using System.Formats.Asn1;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace ClassLibrary
 {
     public static class Faremer
     {
-        static double CorRhombus(double Perimeter)
+        static double CorRhombus(double perimeter)
         {
-            //хаха rhombus phombus
-            Perimeter = Perimeter * 2;
-            return Perimeter;
+            double side = perimeter / 4;
+            double area = side * side;
+            return area;
+        }
+        static double CorCircle(double perimeter)
+        {
+            double radius = perimeter / (2 * 3.141592653);
+            double area = radius * radius * 3.141592653;
+            return area;
         }
     }
 }
