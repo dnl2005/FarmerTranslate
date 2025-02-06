da0b99501312997f40b6877ac653d45a6e9518dd
maggson <dieseverycingein@yandex.ru>
2025-02-06 16:36:57 +0300

    asdgsdg

--- ClassLibrary/Farmer.cs
+++ ClassLibrary/Farmer.cs
@@ -13,7 +13,6 @@ namespace ClassLibrary
         /// <returns> Площадь ромба </returns>
         static double CorRhombus(double perimeter)
         {
-            
             double side = perimeter / 4;
             double area = side * side;
             return area;
@@ -24,19 +23,39 @@ namespace ClassLibrary
             double area = radius * radius * 3.141592653;
             return area;
         }
-        static double Rectangle(double Perimeter)
+        static double CorPentagon(double perimeter)
         {
-            double area = Perimeter * Perimeter / 18;
+            double side = perimeter / 5;
+            double area = (5 * side * side / 2) * Math.Sqrt(5 + 2 * Math.Sqrt(5));
             return area;
         }
-        static double Square(double Perimeter)
+        static double CorHexagon(double perimeter)
         {
-            double area = Math.Pow(Perimeter / 4, 2);
+            double side = perimeter / 6;
+            double area = (3 * Math.Sqrt(3) / 2) * side * side;
             return area;
         }
-        static double Triangle(double Perimeter)
+
+        static double CorRectangle(double perimeter)
         {
-            double area = Math.Sqrt(3) * Math.Pow(Perimeter, 2) / 36;
+            double area = perimeter * perimeter / 18;
+            return area;
+        }
+        static double CorSquare(double perimeter)
+        {
+            double area = Math.Pow(perimeter / 4, 2);
+            return area;
+        }
+        static double CorTriangle(double perimeter)
+        {
+            double area = Math.Sqrt(3) * Math.Pow(perimeter, 2) / 36;
+            return area;
+        }
+
+        static double CorNgon(double perimeter, int sides)
+        {
+            double side = perimeter / sides;
+            double area = (sides * side * side / 4) * (1 / Math.Tan(Math.PI / sides));
             return area;
         }
     }
