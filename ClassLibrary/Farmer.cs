5b1e4972b05301272ff981ecdc865384c1a412d3
maggson <dieseverycingein@yandex.ru>
2025-02-08 14:40:22 +0300

    ыфвпывап

--- ClassLibrary/Farmer.cs
+++ ClassLibrary/Farmer.cs
@@ -1,38 +1,36 @@
-﻿using System.Formats.Asn1;
+﻿using System;
+using System.Data.SqlTypes;
+using System.Formats.Asn1;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.AccessControl;
 using System.Security.Principal;
 
 namespace ClassLibrary
 {
-    public static class Faremer
+    public static class Farmer
     {
-
         /// <summary>
-        /// Метод для нахождения площади ромба
+        /// Метод для нахождения площади ромба с углом в 60 градусов
         /// </summary>
-        /// <param name="perimeter">  Периметр ромба  </param>
-        /// <param name="side">  Сторона ромба  </param>
-        /// <param name="area">  Площадь ромба  </param>
+        /// <param name="perimeter">  Периметр ромба  </param>  
         /// <returns> Возаращет площадь ромба </returns>
-        static double CorRhombus(double perimeter)
+        public static double Rhombus(double perimeter)
         {
-            
-            double side = perimeter / 4;
-            double area = side * side;
+            double corTrianglePerimeter = perimeter / 4 * 3; //сторона 1 из 2 треугольников, из которых состоит ромб
+            double area = CorTriangle(corTrianglePerimeter) * 2; // площадь ромба, s=2*Sтр`ё
             return area;
         }
-
         /// <summary>
         /// Метод для нахождения площади круга
         /// </summary>
         /// <param name="perimeter">  Периметр круга  </param>
-        /// <param name="radius">  Радиус круга  </param>
-        /// <param name="area">  Площадь круга  </param>
         /// <returns> Возаращет площадь круга </returns>
-        static double Circle(double perimeter)
+        public static double Circle(double perimeter)
         {
-            double radius = perimeter / (2 * 3.141592653);
-            double area = radius * radius * 3.141592653;
+            double radius = perimeter / (2 * Math.PI); // радиус круга, r=  p/(2*Пи)
+            double area = Math.Pow(radius, 2) * Math.PI; //площадь ромба, s= r^2*Пи
             return area;
         }
 
@@ -43,11 +41,10 @@ namespace ClassLibrary
         /// <param name="side">  Сторона пятиугольника  </param>
         /// <param name="area">  Площадь пятиугольника  </param>
         /// <returns> Возаращет площадь пятиугольника </returns>
-        static double CorPentagon(double perimeter)
+        public static double CorPentagon(double perimeter)
         {
             double side = perimeter / 5;
-            double area = (5 * side * side / 2) * Math.Sqrt(5 + 2 * Math.Sqrt(5));
-            return area;
+            return 5 * Math.Pow(side, 2) / Math.Tan(Math.PI / 5) / 4;
         }
 
         /// <summary>
@@ -57,52 +54,45 @@ namespace ClassLibrary
         /// <param name="side">  Сторона шестиугольника  </param>
         /// <param name="area">  Площадь шестиугольника  </param>
         /// <returns> Возаращет площадь шестиугольника </returns>
-        static double CorHexagon(double perimeter)
+        public static double CorHexagon(double perimeter)
         {
             double side = perimeter / 6;
-            double area = (3 * Math.Sqrt(3) / 2) * side * side;
-            return area;
+            return 3 * Math.Sqrt(3) * Math.Pow(side, 2) / 2;
         }
 
         /// <summary>
-        /// Метод для нахождения площади прямоугольника
+        /// Метод для нахождения площади прямоугольника, где одна сторона в 2 раза больше другой 
         /// </summary>
         /// <param name="perimeter">  Периметр прямоугольника  </param>
-        /// <param name="side">  Сторона прямоугольника  </param>
-        /// <param name="area">  Площадь прямоугольника  </param>
         /// <returns>  Возаращет площадь прямоугольника  </returns>
-        static double Rectangle(double perimeter)
+        public static double Rectangle(double perimeter)
         {
-            double side = perimeter/6;
-            double area = Math.Pow(side,2)*2;
+            double side = perimeter / 6; //меньшая сторона прямоугольника, получена из формулы P=2(a+2a)=6a => a=P/6
+            double area = side * 2 * side; //площадь прямоугольника, s=a*2a
             return area;
         }
 
         /// <summary>
-        /// Метод для нахождения площади квадрата
+        /// Метод для нахождения площади ``квадрата
         /// </summary>
         /// <param name="perimeter">  Периметр квадрата  </param>
-        /// <param name="side">  Сторона квадрата  </param>
-        /// <param name="area">  Площадь квадрата  </param>
         /// <returns>  Возаращет площадь квадрата  </returns>
-        static double Square(double perimeter)
+        public static double Square(double perimeter)
         {
-            double side = perimeter / 4;
-            double area = Math.Pow(side, 2);
+            double side = perimeter / 4; //сторона квадрата, равна 1/4 периметра 
+            double area = Math.Pow(side, 2); //площадь квадрата, равна его стороне в квадратеё
             return area;
         }
 
         /// <summary>
-        /// Метод для нахождения площади треугольника
+        /// Метод для нахождения площади правильного треугольника
         /// </summary>
-        /// <param name="perimeter">  Периметр треугольника  </param>
-        /// <param name="side">  Сторона треугольника  </param>
-        /// <param name="area">  Площадь треугольника  </param>
-        /// <returns>  Возаращет площадь треугольника  </returns>
-        static double CorTriangle(double perimeter)
+        /// <param name="perimeter">  Периметр правильного треугольника  </param> 
+        /// <returns>  Возаращет площадь правильного треугольника  </returns>
+        public static double CorTriangle(double perimeter)
         {
-            double side = perimeter / 3;
-            double area = Math.Sqrt(3) * Math.Pow(side, 2) / 4;
+            double side = perimeter / 3; //сторона треугольника, a=p/3
+            double area = Math.Sqrt(3) * Math.Pow(side, 2) / 4; //площадь правильного треугольника, s=3^(1/2)*a^2`/4
             return area;
         }
 
@@ -113,11 +103,11 @@ namespace ClassLibrary
         /// <param name="side">  Сторона n-угольника  </param>
         /// <param name="area">  Площадь n-угольника  </param>
         /// <returns>  Возаращет площадь n-уго  </returns>
-        static double CorNgon(double perimeter, int sides)
+        public static double CorNgon(double perimeter, int sides)
         {
             double side = perimeter / sides;
-            double area = (sides * side * side / 4) * (1 / Math.Tan(Math.PI / sides));
-            return area;
+            return sides * Math.Pow(side, 2) / Math.Tan(Math.PI / sides) / 4;
         }
     }
 }
+
