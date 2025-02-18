ae39e5d0c9371212e8c54ff7e08136ce8e7f9ecd
dnl2005 <danek2005w@gmail.com>
2025-02-18 20:25:34 +0300

    added some comments into interface

--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -5,6 +5,12 @@ namespace Interface
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Инициализация и хранение данных о фигурах
+        /// <param name="calculations">Словарь для хранения данных о названии фигуры и способе расчета площади этой фигуры</param>
+        /// <param name="figure">Хранение данных о выбранной фигуре</param>
+        /// <param name="sides">Хранение данных о количестве сторон для н-угольника</param>
+        /// </summary>
         private static Dictionary<string, Func<string, double>> calculations = new Dictionary<string, Func<string, double>>
         {
             { "triangle", Farmer.CorTriangle },
@@ -16,16 +22,20 @@ namespace Interface
             { "hexagon", Farmer.CorHexagon },
             { "nangle", perimeter => Farmer.CorNgon(perimeter, int.Parse(sides)) }
         };
-
         private static string figure = "";
         private static string sides = new Form1().nangleSides.Text;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Метод обработки нажатия на кнопку "Рассчитать"
+        /// </summary>
         private void calculate_Click(object sender, EventArgs e)
         {
+            // обработчик ошибок, связанных с неверным вводом периметра и сторон
             try
             {
                 if (string.IsNullOrEmpty(figure))
@@ -48,7 +58,7 @@ namespace Interface
                         return;
                     }
                 }
-
+                // запись результата в поле "площадь"
                 if (calculations.TryGetValue(figure, out var calculation))
                 {
                     double result = calculation(perimeterInput.Text);
@@ -68,6 +78,9 @@ namespace Interface
             }
         }
 
+        // Обработчик нажатия на элемент списка.
+        // если элемент находится в списке, то происходит выбор фигуры
+        // в случае если выбран н-угольник, проявляет поле для ввода сторон
         private void list_Click(object sender, EventArgs e)
         {
             if (listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < calculations.Count)
@@ -77,7 +90,7 @@ namespace Interface
                 ChangeNSidesVis();
             }
         }
-
+        // метод отрисовки и скрытия поля ввода для сторон н-угольника
         private void ChangeNSidesVis()
         {
             bool isNangle = figure == "nangle";
@@ -85,6 +98,7 @@ namespace Interface
             this.nangleSides.Visible = isNangle;
         }
 
+        // слушатель ввода поля записи сторон н-угольника
         private void nangleSides_TextChanged(object sender, EventArgs e)
         {
             sides = nangleSides.Text;
